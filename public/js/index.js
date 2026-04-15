8d95c4d05dddf8ea1484734e5525ce0095960078
Harsh Jaiswal <harshjaiswal2707@gmail.com>
2026-04-15 20:34:39 +0530

    fix 429 error

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,15 @@
-  let taxSwitch = document.getElementById("switchCheckDefault");
-        taxSwitch.addEventListener("change", () => {
-            let taxInfo = document.getElementsByClassName("tax-info");
-            for (info of taxInfo) {
-                if (info.style.display != "inline") {
-                    info.style.display = "inline";
-                }
-                else {
-                    info.style.display = "none";
-                }
+let taxSwitch = document.getElementById("switchCheckDefault");
 
+if (taxSwitch) {
+    taxSwitch.addEventListener("change", () => {
+        let taxInfo = document.getElementsByClassName("tax-info");
+
+        for (let info of taxInfo) {
+            if (info.style.display != "inline") {
+                info.style.display = "inline";
+            } else {
+                info.style.display = "none";
             }
-        });
+        }
+    });
+}
