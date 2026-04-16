00fd13d51323da958da2655526aa63204523fab6
Harsh Jaiswal <harshjaiswal2707@gmail.com>
2026-04-16 06:23:22 +0530

    fix merge conflict in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,4 @@
 let taxSwitch = document.getElementById("switchCheckDefault");
-<<<<<<< HEAD
 
 if (taxSwitch) {
     taxSwitch.addEventListener("change", () => {
@@ -14,16 +13,3 @@ if (taxSwitch) {
         }
     });
 }
-=======
-taxSwitch.addEventListener("change", () => {
-    let taxInfo = document.getElementsByClassName("tax-info");
-    for (info of taxInfo) {
-        if (info.style.display != "inline") {
-            info.style.display = "inline";
-        }
-        else {
-            info.style.display = "none";
-        }
-    }
-});
->>>>>>> features
